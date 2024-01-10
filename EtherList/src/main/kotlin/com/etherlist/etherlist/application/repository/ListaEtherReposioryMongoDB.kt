d51693e762e6fad6112354ee8f67ba49ad4e01af
mateus-damasceno-zup <mateus.damasceno@zup.com.br>
2024-01-09 23:33:14 -0300

    corrigido application properties

--- EtherList/src/main/kotlin/com/etherlist/etherlist/application/repository/ListaEtherReposioryMongoDB.kt
+++ EtherList/src/main/kotlin/com/etherlist/etherlist/application/repository/ListaEtherReposioryMongoDB.kt
@@ -3,7 +3,4 @@ package com.etherlist.etherlist.application.repository
 import com.etherlist.etherlist.adapter.input.v1.controller.request.model.ListaEtherRequest
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface ListaEtherReposioryMongoDB:MongoRepository<ListaEtherRequest,String> {
-
-
-}
+interface ListaEtherReposioryMongoDB:MongoRepository<ListaEtherRequest,String>
